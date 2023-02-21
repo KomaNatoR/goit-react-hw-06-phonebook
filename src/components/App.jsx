0d9d8266c901d4e85ca25f4f1841386cfabaed48
Serhii Markov <goo.komalel@gmail.com>
2023-02-21 09:44:03 +0200

    якась хрінь!

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,56 +1,93 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { addContact, deleteContact } from "./redux/action";
 
 import FormikForm from "./Form";
 import Filter from "./Filter";
 import ContactList from "./ContactList";
 import { MainDiv } from "./app.styled";
 
-const template = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
+// const template = [
+//   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+// ];
 
+// export const App = () => {
+//   const [contacts, setContacts] = useState(()=>
+//     JSON.parse(window.localStorage.getItem('contacts')) ?? template
+//   );
+//   const [filter, setFilter] = useState('');
+
+//   useEffect(() => {
+//     window.localStorage.setItem('contacts', JSON.stringify(contacts));
+//   },[contacts])
+// // ---------------------------------------------------------||   ФУНКЦІЇ ОБРОБКИ   ||
+//   const addContact = (contact) => {
+//     const personNormalize = contact.name.toLowerCase().trim();
+//     const contactsMap = contacts.find(cont => cont.name.toLowerCase() === personNormalize);
+
+//     if (contactsMap) return alert("Це хіба можна так робити?");
+//     setContacts(prevConts=>[contact,...prevConts]);
+//   };
+//   const deleteContact = (id) => {
+//     // const deleteCont = contacts.filter(cont => cont.id !== id);
+//     // setContacts(deleteCont);
+//     setContacts(prevCont=>prevCont.filter(cont => cont.id !== id));
+//   };
+//   const onFilterChange = (e) => {
+//     const { value } = e.target;
+
+//     setFilter(value);
+//   };
+//   const normalizeFilter = filter.toLowerCase().trim();
+//   const visiblePersons = contacts.filter(cont=>cont.name.toLowerCase().includes(normalizeFilter));
+
+//   // ---------------------------------------------------------||   РЕНДЕР   ||
+//   return (
+//     <MainDiv>
+//       <h1>Phonebook</h1>
+//       <FormikForm hendleSubmit={addContact} />
+      
+//       <h2>Contacts</h2>
+//       <Filter onFilterChange={onFilterChange} value={filter} />
+//       <ContactList onClick={deleteContact} contacts={visiblePersons} />
+//     </MainDiv>
+//   );
+// };
+// ---------------------------------------------------------- || ReWorking
 export const App = () => {
-  const [contacts, setContacts] = useState(()=>
-    JSON.parse(window.localStorage.getItem('contacts')) ?? template
-  );
-  const [filter, setFilter] = useState('');
+  const contacts = useSelector(store => store.contacts);
 
-  useEffect(() => {
-    window.localStorage.setItem('contacts', JSON.stringify(contacts));
-  },[contacts])
-// ---------------------------------------------------------||   ФУНКЦІЇ ОБРОБКИ   ||
-  const addContact = (contact) => {
+  const dispatch = useDispatch();
+
+  const onAddContact = (contact) => {
     const personNormalize = contact.name.toLowerCase().trim();
-    const contactsMap = contacts.find(cont => cont.name.toLowerCase() === personNormalize);
+    const contactsFind = contacts.find(cont => cont.name.toLowerCase() === personNormalize);
 
-    if (contactsMap) return alert("Це хіба можна так робити?");
-    setContacts(prevConts=>[contact,...prevConts]);
+    if (contactsFind) return alert("Це хіба можна так робити?");
+    // setContacts(prevConts=>[contact,...prevConts]);
+    
+    const action = addContact(contact);
+    dispatch(action);
   };
-  const deleteContact = (id) => {
-    // const deleteCont = contacts.filter(cont => cont.id !== id);
-    // setContacts(deleteCont);
-    setContacts(prevCont=>prevCont.filter(cont => cont.id !== id));
+  const onDeleteContact = (id) => {
+    const action = deleteContact(id);
+    dispatch(action);
   };
-  const onFilterChange = (e) => {
-    const { value } = e.target;
 
-    setFilter(value);
-  };
-  const normalizeFilter = filter.toLowerCase().trim();
-  const visiblePersons = contacts.filter(cont=>cont.name.toLowerCase().includes(normalizeFilter));
-
-  // ---------------------------------------------------------||   РЕНДЕР   ||
   return (
     <MainDiv>
       <h1>Phonebook</h1>
-      <FormikForm hendleSubmit={addContact} />
+      <FormikForm hendleSubmit={onAddContact} />
       
       <h2>Contacts</h2>
-      <Filter onFilterChange={onFilterChange} value={filter} />
-      <ContactList onClick={deleteContact} contacts={visiblePersons} />
+      <Filter onFilterChange={"#"} value={"#"} />
+      <ContactList onClick={onDeleteContact} contacts={contacts} />
     </MainDiv>
   );
 };
+
+
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,13 +1,16 @@
 import PropTypes from 'prop-types';
 
-const ContactList = ({contacts, onClick}) => {
+const ContactList = ({ contacts, onClick }) => {
+
     return (
         <ul>
-            {contacts.map(cont => (<li key={cont.id}>
-                <span>{cont.name}:</span>
-                <span>{cont.number}</span>
-                <button onClick={()=>onClick(cont.id)}>Delete</button>
-            </li>))}
+            {contacts.map(cont => (
+                <li key={cont.id}>
+                    <span>{cont.name}:</span>
+                    <span>{cont.number}</span>
+                    <button onClick={()=>onClick(cont.id)}>Delete</button>
+                </li>
+            ))}
         </ul>
     );
 };
